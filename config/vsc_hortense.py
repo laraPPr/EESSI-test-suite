61759cc23444f7ae95e51170e5fb7aed438217a4
Lara Ramona Peeters <49882639+laraPPr@users.noreply.github.com>
2023-08-08 14:06:20 +0200

    Update vsc_hortense.py

--- config/vsc_hortense.py
+++ config/vsc_hortense.py
@@ -6,7 +6,7 @@
 from reframe.core.backends import register_launcher
 from reframe.core.launchers import JobLauncher
 
-account = "gadminforever"
+account = ""
 
 # use 'info' to log to syslog
 syslog_level = 'warning'
